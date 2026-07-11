d8d4c9f2bccd668625f05769719fe2484328f627
praneeth1808 <praneeth3331997@gmail.com>
2026-07-11 11:29:24 -0400

    Initial SwiftUI project setup

---
+++ SwipeMTW/SwipeMTWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeMTWApp.swift
+//  SwipeMTW
+//
+//  Created by DV Praneeth on 7/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SwipeMTWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
